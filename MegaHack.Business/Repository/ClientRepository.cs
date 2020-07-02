bc44ae3004b522f1694740cecbf84ec98fecf10d
Gustavo Dias <gustavodias2@gmail.com>
2020-07-02 02:46:14 -0300

    Adicionando novo repositorio para controle da classe de clientes

---
+++ MegaHack.Business/Repository/ClientRepository.cs
@@ -0,0 +1,21 @@
+﻿using MegaHack.Business.Context;
+using MegaHack.Business.ViewModel;
+using MegaHack.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MegaHack.Business.Repository
+{
+
+    private readonly MegaHackContext _context;
+
+    public ClientRepository(MegaHackContext context)
+    {
+        _context = context;
+    }
+
+    public class ClientRepository
+    {
+    }
+}
