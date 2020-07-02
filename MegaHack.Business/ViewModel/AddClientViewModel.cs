de3e223f6289e07d82bafd91938568dba26f9ab9
Gustavo Dias <gustavodias2@gmail.com>
2020-07-02 02:48:09 -0300

    Adicionando nova view de acesso aos dados do cliente

---
+++ MegaHack.Business/ViewModel/AddClientViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MegaHack.Business.ViewModel
+{
+    public class AddClientViewModel
+    {
+
+        public int ClientID { get; set; }
+
+        public string ClientName { get; set; }
+
+        public string ClientNickName { get; set; }
+
+        public string ClientLogin { get; set; }
+
+        public string ClientPassword { get; set; }
+
+        public string ClientEmail { get; set; }
+
+        public DateTime ClientDateBirth { get; set; }
+
+        public int ClientAge { get; set; }
+
+    }
+}
