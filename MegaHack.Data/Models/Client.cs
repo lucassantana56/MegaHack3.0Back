b06ac3114400d2acea0e58ffa318f8feb9560abb
Gustavo Dias <gustavodias2@gmail.com>
2020-07-02 02:51:08 -0300

    Atualizando model de cliente

--- MegaHack.Data/Models/Client.cs
+++ MegaHack.Data/Models/Client.cs
@@ -7,13 +7,23 @@ namespace MegaHack.Data.Models
 {
     public class Client
     {
-        [Key]
-        public int ClientID { get; set; }
+        [Key] public int ClientID { get; set; }
 
-        [Required]
-        public string ClientName { get; set; }
+        [Required] public string Name { get; set; }
 
+        public string NickName { get; set; }
 
+        [Required] public string Login { get; set; }
+
+        [Required] public string Password { get; set; }
+
+        [Required] public string Email { get; set; }
+
+        [Required] public ICollection<Contact> Contacts { get; set; }
+
+        [Required] public DateTime DateBirth { get; set; }
+
+        [Required] public int Age { get; set; }
 
     }
 }
