0187844e6b870fe75bfbe32e33916fcdf3ccc471
Gustavo Dias <gustavodias2@gmail.com>
2020-07-02 02:48:44 -0300

    Adicionando controller de cliente

---
+++ MegaHack.WebApi/Controllers/ClientController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MegaHack.Business.BusinessModels;
+using MegaHack.Business.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MegaHack.WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly ClientRepository _clientRepository;
+        private readonly ClientBLL _clientBLL;
+
+        public ClientController(ClientRepository clientRepository, ClientBLL clientBLL)
+        {
+            _clientRepository = clientRepository;
+            _clientBLL = clientBLL;
+        }
+    }
+}
